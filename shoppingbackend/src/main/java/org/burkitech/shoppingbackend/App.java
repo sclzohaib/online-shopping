4fcab6d466b498b7c9d35864e85f21a963fbbb31
sclzohaib <sclzohaib@gmail.com>
2018-03-13 14:20:11 +0500

    My first project commit successfully..

---
+++ shoppingbackend/src/main/java/org/burkitech/shoppingbackend/App.java
@@ -0,0 +1,13 @@
+package org.burkitech.shoppingbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
