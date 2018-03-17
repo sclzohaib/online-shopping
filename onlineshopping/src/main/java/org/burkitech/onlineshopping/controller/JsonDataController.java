f230564a13e3e6dc997c9dbd1ce6e1a797067457
sclzohaib <sclzohaib@gmail.com>
2018-03-17 07:07:25 +0500

    17-3-2018 (7.07am)

--- onlineshopping/src/main/java/org/burkitech/onlineshopping/controller/JsonDataController.java
+++ onlineshopping/src/main/java/org/burkitech/onlineshopping/controller/JsonDataController.java
@@ -1,9 +1,33 @@
 package org.burkitech.onlineshopping.controller;
 
+import java.util.List;
+
+import org.burkitech.shoppingbackend.dao.ProductDAO;
+import org.burkitech.shoppingbackend.dto.Product;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/json/data")
 public class JsonDataController {
+	@Autowired
+	private ProductDAO productDAO;
+
+	@RequestMapping("/all/products")
+	@ResponseBody
+	public List<Product> getAllProducts() {
+		return productDAO.listActiveProducts();
+
+	}
+
+	@RequestMapping("/category/{id}/products")
+	@ResponseBody
+	public List<Product> getAllProductsByCategory(@PathVariable int id) {
+		return productDAO.listActiveProductsByCategory(id);
+
+	}
+
 }
