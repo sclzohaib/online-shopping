593d33a57c0377574f7d94c956596e53f3f538ee
sclzohaib <sclzohaib@gmail.com>
2018-03-17 05:56:29 +0500

    5.56am 17-03-2018

--- shoppingbackend/src/main/java/org/burkitech/shoppingbackend/config/HibernateConfig.java
+++ shoppingbackend/src/main/java/org/burkitech/shoppingbackend/config/HibernateConfig.java
@@ -14,15 +14,14 @@ import org.springframework.orm.hibernate5.LocalSessionFactoryBuilder;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
-@ComponentScan(basePackages = {"org.burkitech.shoppingbackend.dto"})
+@ComponentScan(basePackages = { "org.burkitech.shoppingbackend.dto" })
 @EnableTransactionManagement
 public class HibernateConfig {
-	
-	private final static String DATABASE_URL = "jdbc:oracle:thin:192.168.100.11:1521:xe";
+	private final static String DATABASE_URL = "jdbc:oracle:thin:@192.168.100.11:1521:xe";
 	private final static String DATABASE_DRIVER = "oracle.jdbc.driver.OracleDriver";
 	private final static String DATABASE_USERNAME = "SPRINGOSS";
 	private final static String DATABASE_PASSWORD = "oracle";
-	private final static String DATABASE_DIALECT = "org.hibernate.dialect.Oracle11gDialect";
+	private final static String DATABASE_DIALECT = "org.hibernate.dialect.Oracle10gDialect";
 
 	// Data Source bean will be available
 	@Bean
@@ -41,22 +40,23 @@ public class HibernateConfig {
 	public SessionFactory getSessionFactory(DataSource dataSource) {
 		LocalSessionFactoryBuilder builder = new LocalSessionFactoryBuilder(dataSource);
 		builder.addProperties(getHibernateProperties());
+		builder.scanPackages("org.burkitech.shoppingbackend.dto");
 		return builder.buildSessionFactory();
 	}
 
 	// all the hibernate properties will be return in this method
-	public Properties getHibernateProperties() {
+	private Properties getHibernateProperties() {
 		Properties properties = new Properties();
 		properties.put("hibernate.dialect", DATABASE_DIALECT);
 		properties.put("hibernate.show_sql", true);
 		properties.put("hibernate.format_sql", true);
 		return properties;
 	}
-	
+
 	@Bean
-	public HibernateTransactionManager getTransactionManager(SessionFactory sessionFactory){
-		HibernateTransactionManager transactionManager=new HibernateTransactionManager(sessionFactory);		
+	public HibernateTransactionManager getTransactionManager(SessionFactory sessionFactory) {
+		HibernateTransactionManager transactionManager = new HibernateTransactionManager(sessionFactory);
 		return transactionManager;
-		}
+	}
 
 }
--- shoppingbackend/src/main/java/org/burkitech/shoppingbackend/dto/Category.java
+++ shoppingbackend/src/main/java/org/burkitech/shoppingbackend/dto/Category.java
@@ -5,14 +5,16 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
+//@Table(name = "CATEGORY")
 public class Category {
 	/*
 	 * Private Fields
 	 */
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+@Id
+//	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
 
 	private String name;
