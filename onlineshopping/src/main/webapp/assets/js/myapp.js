00c0254397722d4f9b08c756e08a6d5b30f33f5d
sclzohaib <sclzohaib@gmail.com>
2018-03-29 12:32:12 +0500

    wahaj

--- onlineshopping/src/main/webapp/assets/js/myapp.js
+++ onlineshopping/src/main/webapp/assets/js/myapp.js
@@ -29,6 +29,17 @@ $(function() {
 
 	}
 
+	// tackle the csrf token
+	var token =$('meta[name="_csrf"]').attr('content');
+	var header =$('meta[name="_csrf_header"]').attr('content');
+
+	if(token.length >0 && header.length >0){
+	//set token header for ajax request
+	$(document).ajaxSend(function(e,xhr,options){
+	zhr.setRequestHeader(header,token);
+	});
+	}
+	
 	// code for jquery datatable
 
 	var $table = $('#productListTable');
@@ -314,5 +325,37 @@ $(function() {
 					}
 				});
 	}
-
+	// ------------------------------------------------------------------
+	// validation code for userform
+	var $loginForm = $('#loginForm');
+	if ($loginForm.length) {
+		$loginForm
+				.validate({
+					rules : {
+						username : {
+							required : true,
+							email:true
+						},
+						password : {
+							required : true
+						}
+					},
+					messages : {
+						username : {
+							required : 'Please enter username!',
+							email : 'Please enter valid email address!'
+						},
+						password : {
+							required : 'Please enter password!'
+						}
+					},
+					errorElement : 'em',
+					errorPlacement : function(error, element) {
+						// add the class of help-block
+						error.addClass('help-block');
+						// add the error element after the input element
+						error.insertAfter(element);
+					}
+				});
+	}
 });
